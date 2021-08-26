825af31cc913fd965786b81793aba66fa1567a3b
David Tan <david.tan@davidyytan.com>
2021-08-26 17:27:48 +0800

    change cert validity to 3650 days

--- generate-certificate.sh
+++ generate-certificate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-openssl req -x509 -newkey rsa:4096 -nodes -out /ssl_certs/server.crt -keyout /ssl_certs/server.key -days 365 -subj "/C=SG/ST=Singapore/L=Singapore/O=NA/OU=NA/CN=localhost"
+openssl req -x509 -newkey rsa:4096 -nodes -out /ssl_certs/server.crt -keyout /ssl_certs/server.key -days 3650 -subj "/C=SG/ST=Singapore/L=Singapore/O=NA/OU=NA/CN=localhost"
